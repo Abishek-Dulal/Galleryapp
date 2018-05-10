032a8153b8be44ef4d834df9190ae61567d77c4c
DESKTOP-42S1U5O\user <abishek.dulal666s@gmail.com>
2018-05-10 06:52:03 +0545

    add gallery js file

--- gallery.js
+++ gallery.js
@@ -2,4 +2,4 @@
 let api_key="f2345aa7d81b85b6865bc7698d7e6f06";
 let secret="9daf2cc6e1fa4be1";
 
-let url="";
+let url=`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api_key}&format=json&text=cat&page=2&per_page=20&nojsoncallback=1`;
